870d3eacd542614139ef898b225cabcc313d2377
adithya.adi <adithya.adi1508@gmail.com>
2026-01-18 20:35:44 +0530

    - added line which prevents running DataInitializer.java in testing

--- Java/src/main/java/com/project/task_manager/DataInitializer.java
+++ Java/src/main/java/com/project/task_manager/DataInitializer.java
@@ -6,10 +6,12 @@ import com.project.task_manager.repository.RoleRepository;
 import com.project.task_manager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class DataInitializer implements CommandLineRunner {
 
     @Autowired
